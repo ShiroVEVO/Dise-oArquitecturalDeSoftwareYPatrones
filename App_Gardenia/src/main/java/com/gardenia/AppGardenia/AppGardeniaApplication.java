2b2b06703af5d8ab426d079d1d0adce093456778
Adrianシロ <adrianstratos@gmail.com>
2024-10-29 18:57:19 -0500

    estructura básica MVP

---
+++ App_Gardenia/src/main/java/com/gardenia/AppGardenia/AppGardeniaApplication.java
@@ -0,0 +1,13 @@
+package com.gardenia.AppGardenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGardeniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGardeniaApplication.class, args);
+	}
+
+}
